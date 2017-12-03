d2b714a8672d888f9da5bd997c81976d996ec126
trevormalloy <trevoramalloy@gmail.com>
2017-12-03 12:54:34 -0800

    hooking up openweather api

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+import axios from 'axios';
 
-export default class SearchBar extends Component {
-constructor(props){
-    super(props);
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
 
-    this.state = {term: '' };
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
 }
 
-onInputChange(event) {
-    console.log(event.target.value);
-    this.setState({ term: event.target.value });
-}
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // Fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
+
+    }
 
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
                     placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
@@ -27,3 +42,9 @@ onInputChange(event) {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
