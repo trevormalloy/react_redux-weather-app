9843d4d54d863281d53c6a06d3bab29149ef6852
trevormalloy <trevoramalloy@gmail.com>
2017-12-03 23:37:26 -0800

    added some styling, and google maps to the app

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,19 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function avaerage(data) {
+    return _.round(_.sum(data)/data.length);
+}
 
 export default (props) => {
     return (
         <div>
             <Sparklines height={100} width={150} data={props.data}>
                 <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
             </Sparklines>
+            <div>{avaerage(props.data)} {props.units}</div>
         </div>
     );
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
-
+import GoogleMap from '../components/google_map';
 
 // Get the data for each city
 class WeatherList extends Component {
@@ -10,19 +10,15 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord;
+
 
         return (
             <tr key={name}>
-                <td>{name}</td>
-                <td className="temps">
-                    <Chart data={temps} color="orange" />
-                </td>
-                <td>
-                    <Chart data={pressures} color="green" />
-                </td>
-                <td>
-                    <Chart data={humidities} color="black" />
-                </td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={pressures} color="green" units="hPa"/></td>
+                <td><Chart data={humidities} color="black" units="%"/></td>
             </tr>
         );
     }
@@ -34,9 +30,9 @@ class WeatherList extends Component {
                     <thead>
                         <tr>
                             <th>City</th>
-                            <th>Temperature</th>
-                            <th>Pressure</th>
-                            <th>Humidity</th>
+                            <th>Temperature (K)</th>
+                            <th>Pressure (hPa)</th>
+                            <th>Humidity (%)</th>
                         </tr>
                     </thead>
                     <tbody>
