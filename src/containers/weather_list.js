cb45dbda633e88d3c7a9918bf12049362f0eb72d
trevormalloy <trevoramalloy@gmail.com>
2017-12-03 20:08:09 -0800

    adding charts for the weather data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
+
+// Get the data for each city
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td className="temps">
+                    <Chart data={temps} color="orange" />
+                </td>
+                <td>
+                    <Chart data={pressures} color="green" />
+                </td>
+                <td>
+                    <Chart data={humidities} color="black" />
+                </td>
             </tr>
         );
     }
 
     render() {
         return (
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.weather.map(this.renderWeather)}
-                </tbody>
-            </table>
+            <div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Temperature</th>
+                            <th>Pressure</th>
+                            <th>Humidity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.weather.map(this.renderWeather)}
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
